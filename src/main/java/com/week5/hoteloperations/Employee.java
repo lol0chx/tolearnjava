c58eb4d103dc1298f7d0427ce69b250481454567
lol0chx <henoksmekuria@gmail.com>
2025-10-21 13:55:21 -0700

    update

--- src/main/java/com/week5/hoteloperations/Employee.java
+++ src/main/java/com/week5/hoteloperations/Employee.java
@@ -1,11 +1,14 @@
 package com.week5.hoteloperations;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Employee {
     private long employeeId;
     private String name;
     private String department;
     private double payRate;
-    private double hoursWorked;
+    private double hoursWorked=0;
     private double startTime;
 
     public Employee(long employeeId, String name, String department, double payRate, double hoursWorked, double startTime) {
@@ -15,6 +18,10 @@ public class Employee {
         this.payRate = payRate;
         this.hoursWorked = hoursWorked;
         this.startTime = startTime;
+    }
+
+    public Employee() {
+
     }
 
     public long getEmployeeId() {
@@ -81,7 +88,24 @@ public class Employee {
     public void punchIn(double time) {
         startTime = time;
     }
+    LocalTime time = LocalTime.now();
+    LocalDate date = LocalDate.now();
+    double timeC;
+    double clockInTime;
+    public void punchIn(){
+        startTime= time.getHour()+ (time.getMinute()/60);
+        clockInTime= startTime;
+        System.out.println(timeC);
+        System.out.println(time);
+    }
+    public void punchOut(){
+        double currentTime =time.getHour()+ (time.getMinute()/60);
 
+        hoursWorked= hoursWorked+ (  timeC- startTime);
+        System.out.println("time right now :"+ timeC);
+        System.out.println("hours worked " +hoursWorked);
+        System.out.println("start time "+ startTime);
+    }
     public void punchOut(double time) {
         hoursWorked += (time - startTime);
     }
